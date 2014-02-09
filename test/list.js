0e2cb8935236d689a8f8901203cd1220e29edf74
Martin Wintz <martin.wintz@gmail.com>
2014-02-08 21:46:46 -0500

    Added test for querying strings.

--- test/list.js
+++ test/list.js
@@ -33,5 +33,29 @@ module.exports = function () {
     });
   });
 
+  describe('/user?name=Eric Cartman', function () {
+    it('should respond with an array of all users', function (done) {
+
+      var users = fixtures.users;
+
+      request(URL)
+        .get('/user?name=Eric Cartman')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) throw err;
+
+          res.body.should.be.an.instanceOf(Array)
+            .with.lengthOf(1);
+          res.body[0].should.have.property('_id');
+          res.body[0].should.have.property('name');
+          res.body[0].should.have.property('joinDate');
+          res.body[0].name.should.equal('Eric Cartman');
+
+          done();
+        });
+
+    });
+  });
+
 };
 
